880ac1ea763863662cfaee1afb3aa526d323a71e
Kirill Litvinov <aseeks@yandex.ru>
2022-11-23 20:08:47 +0300

    Cleaned the code in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -2,42 +2,19 @@ import modules
 import argparse
 import os
 
-# This file should not be called by the user. The registry uses this file.
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		'-qt', '--qtype', help='Probably the most useful sort. Sorts files based on filegroups. For example, .txt and '
-		                       '.docx would be moved to the Documents folder.', action='store_true')
-	parser.add_argument(
-		'-t', '--type', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                     'to the Type folder.', action='store_true')
-	parser.add_argument(
-		'-tb', '--typeb', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                       'to the Type folder.', action='store_true')
-	parser.add_argument(
-		'-to', '--typemto', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                         'to the Type folder.', action='store_true')
-	parser.add_argument(
-		'-tbo', '--typebmto', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                           'to the Type folder.', action='store_true')
-	parser.add_argument(
-		'-qe', '--qextension', help='Sorts files purely by extension.', action='store_true')
-
-	parser.add_argument(
-		'-e', '--ext', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                    'to the Extension folder.', action='store_true')
-	parser.add_argument(
-		'-eb', '--extb', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                      'to the Extension folder.', action='store_true')
-	parser.add_argument(
-		'-eo', '--extmto', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                        'to the Extension folder.', action='store_true')
-	parser.add_argument(
-		'-ebo', '--extbmto', help='Sorts files based on custom filegroups. Checked extensions would be moved '
-		                          'to the Extension folder.', action='store_true')
-	parser.add_argument(
-		'-x', '--extract', help='Uproots ALL files in sub directories to current directory. Be careful, this will '
-		                        'clear out any and all subfolders', action='store_true')
+	parser.add_argument('-qt', '--qtype')
+	parser.add_argument('-t', '--type')
+	parser.add_argument('-tb', '--typeb')
+	parser.add_argument('-to', '--typemto')
+	parser.add_argument('-tbo', '--typebmto')
+	parser.add_argument('-qe', '--qextension')
+	parser.add_argument('-e', '--ext')
+	parser.add_argument('-eb', '--extb')
+	parser.add_argument('-eo', '--extmto')
+	parser.add_argument('-ebo', '--extbmto')
+	parser.add_argument('-x', '--extract')
 
 	args = vars(parser.parse_args())
 
